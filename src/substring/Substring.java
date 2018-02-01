034e519bc4247a8dee958ec768f7bce67b99e144
Donny Rozendal <donny.rozendal@hva.nl>
2018-02-01 16:21:32 +0100

    Boyer Moore

--- src/substring/Substring.java
+++ src/substring/Substring.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Donny Rozendal
@@ -16,24 +18,26 @@ import java.io.IOException;
 public class Substring {
 
     public static void main(String[] args) {
-        String pattern = "bier";
-        String book = readBook();
+        List<String> words = new ArrayList<>();
+        words.add("oog");
+        words.add("golven");
+        words.add("bloemen");
+        words.add("lijstervink");
+        words.add("poster");
+        words.add("haar");
+        words.add("een");
+        words.add("zonnelicht");
+        words.add("wonderbaar");
+        words.add("bier");
         
-        KMP kmp = new KMP(pattern);
-        int counter = 0;
-        int start = 0;
-        int offset = kmp.search(book, start);
-        while (offset != book.length()) {
-            counter++;
-            start = offset;
-            offset = kmp.search(book, start + pattern.length() + 1);
+        for (String word : words) {
+            searchKMP(word);
+            searchBoyerMoore(word);
         }
-        System.out.println("The specified word is found " + counter + " times.");
-        System.out.println("There were " + KMP.charComparisons + " comparisons.");
     }
     
     public static String readBook() {
-        File file = new File("/home/terazeus/Dropbox/College/Jaar 2/Sorting and searching/Week 6/mei_herman_gorter.txt");
+        File file = new File("mei_herman_gorter.txt");
         BufferedInputStream bis = null;
         FileInputStream fis = null;
         StringBuilder sb = new StringBuilder();
@@ -60,6 +64,40 @@ public class Substring {
             }
         }
         return sb.toString();
+    }
+    
+    public static void searchKMP(String pattern) {
+        String book = readBook();
+        KMP kmp = new KMP(pattern);
+        KMP.charComparisons = 0;
+        int counter = 0;
+        int start = 0;
+        int offset = kmp.search(book, start);
+        while (offset != book.length()) {
+            counter++;
+            start = offset;
+            offset = kmp.search(book, start + pattern.length() + 1);
+        }
+        System.out.println(pattern);
+        System.out.println("The specified word is found " + counter + " times.");
+        System.out.println("KMP: " + KMP.charComparisons + " comparisons.\n");
+    }
+    
+    public static void searchBoyerMoore(String pattern) {
+        String book = readBook();
+        BoyerMoore bm = new BoyerMoore(pattern);
+        BoyerMoore.charComparisons = 0;
+        int counter = 0;
+        int start = 0;
+        int offset = bm.search(book, start);
+        while (offset != book.length()) {
+            counter++;
+            start = offset;
+            offset = bm.search(book, start + pattern.length() + 1);
+        }
+        System.out.println(pattern);
+        System.out.println("The specified word is found " + counter + " times.");
+        System.out.println("Boyer-Moore: " + BoyerMoore.charComparisons + " comparisons.\n");
     }
     
 }
